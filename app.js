300142c204c24cf61ebe8ce501e31c90d1959809
nichodaenielle <daenielledaypuyart@gmail.com>
2026-06-09 14:31:39 +0800

    Fix: Remove duplicate server start that was causing double initialization

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
 // Master entry point for Hostinger / Phusion Passenger
 console.log('--- Hostinger: Starting Server ---');
 require('./backend/server.js');
-// Master entry point for Hostinger / Phusion Passenger
-require('./backend/server.js');
